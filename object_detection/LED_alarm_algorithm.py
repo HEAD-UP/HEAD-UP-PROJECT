5d9de1363b67f6c9a2a63ae3eaeeb19b437d5cdf
KimJune0 <john5910@naver.com>
2020-06-08 22:09:55 +0900

    change LED_alarm_algorithm and add deprecated object detection local

--- object_detection/LED_alarm_algorithm.py
+++ object_detection/LED_alarm_algorithm.py
@@ -2,58 +2,62 @@ import threading
 import RPi.GPIO as GPIO
 import time
 
-
 def warning_on():
-    global timer, stime, sign_is_on
-    while True:
-        if timer > 0:
-            print("timer : " + str(timer))
-            sign_is_on = True
-            stime = time.time()
-            GPIO.output(10, True)
-            time.sleep(timer)
-            GPIO.output(10, False)
-            sign_is_on = False
-        else:
-            pass
-
-def input_cmd():
-    global danger
-    while True:
-        num = int(input("put input : "))
-        if num == 1:
-            danger = True
-        elif num == 2:
-            danger = False
-        else:
-            break
-
+    global timer, danger, end
+    
+    while not end:
+        if danger:
+            timer = 5
             
-def init(danger):
-    #9 : green 10 : red 11: blue
+        elif timer == 0:
+            GPIO.output(10, False)
+        
+        if timer > 0:
+            print("Time : ", timer)
+            GPIO.output(10, True)
+            time.sleep(1)
+            timer -= 1
+            
+    GPIO.output(10, False)
+
+def socket_input(num):
+    global danger
+    if int(num) == 1:
+        danger = True
+    elif int(num) == 0:
+        danger = False
+
+def thread_end(t):    
+    global end
+    
+    end = True
+    t.join()
+    print("thread end")
+    
+def init():
+    global timer, danger, end
+    #9 : green # 10 : red # 11: blue
     GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
     GPIO.setup(9, GPIO.OUT)
     GPIO.setup(10, GPIO.OUT)
-
+    
+    end = False
+    danger = False
     timer = 0
-    stime = 0
-    sign_is_on = False
 
     t = threading.Thread(target=warning_on, args = ())
-    std = threading.Thread(target=input_cmd, args = ())
-
-    gtime = time.time()
-
     t.start()
-    std.start()
+    return t
 
+
+if __name__ == '__main__':
+    th_input = init()
     while True:
-        if danger and not sign_is_on:
-            timer = 5
-        elif danger and sign_is_on:
-            dtime = time.time()
-        elif not danger and sign_is_on:
-            timer = int(dtime) - int(stime)
-        # not danger and not sign_is_on : pass
-
+        temp = input()
+        if temp == 3:
+            break
+        else :
+            socket_input(temp)
+    thread_end(th_input)
 
