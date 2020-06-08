2cdad866670414a3de8fa1775485d2385978176e
KimJune0 <john5910@naver.com>
2020-06-08 17:25:04 +0900

    led + object_detection on 라즈비안
    led + object_detection on 라즈비안

--- object_detection/LED_alarm_algorithm.py
+++ object_detection/LED_alarm_algorithm.py
@@ -2,10 +2,6 @@ import threading
 import RPi.GPIO as GPIO
 import time
 
-#9 : green 10 : red 11: blue
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(9, GPIO.OUT)
-GPIO.setup(10, GPIO.OUT)
 
 def warning_on():
     global timer, stime, sign_is_on
@@ -32,40 +28,32 @@ def input_cmd():
         else:
             break
 
-danger = False #false = not danger / true = danger
-timer = 0
-stime = 0
-sign_is_on = False
-gtime = time.time()
+            
+def init(danger):
+    #9 : green 10 : red 11: blue
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(9, GPIO.OUT)
+    GPIO.setup(10, GPIO.OUT)
 
-t = threading.Thread(target=warning_on, args = ())
-std = threading.Thread(target=input_cmd, args = ())
+    timer = 0
+    stime = 0
+    sign_is_on = False
 
-t.start()
-std.start()
+    t = threading.Thread(target=warning_on, args = ())
+    std = threading.Thread(target=input_cmd, args = ())
 
-danger = True
+    gtime = time.time()
 
-while True:
-    if danger and not sign_is_on:
-        timer = 5
-    elif danger and sign_is_on:
-        dtime = time.time()
-    elif not danger and sign_is_on:
-        timer = int(dtime) - int(stime)
-    # not danger and not sign_is_on : pass
-    
-    '''
-    if danger:
-        if sign_is_on:
-            dtime = time.time()
-        else:
+    t.start()
+    std.start()
+
+    while True:
+        if danger and not sign_is_on:
             timer = 5
-            sign_is_on = True  
-    else:
-        if sign_is_on:
-            timer = int(dtime) - int(stime)  
-        else:
-            pass
-            '''
+        elif danger and sign_is_on:
+            dtime = time.time()
+        elif not danger and sign_is_on:
+            timer = int(dtime) - int(stime)
+        # not danger and not sign_is_on : pass
+
 
